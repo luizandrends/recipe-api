18c11df466699189eae04f38ed97fb96d1609ec5
Luiz Andre <luizxd234@gmail.com>
2020-10-15 11:09:32 -0300

    feat: unit tests

--- __tests__/unit/parseStringToArray.spec.ts
+++ __tests__/unit/parseStringToArray.spec.ts
@@ -1,4 +1,4 @@
-import parseStringToArray from '../src/modules/recipes/utils/parseStringToArray';
+import parseStringToArray from '../../src/modules/recipes/utils/parseStringToArray';
 
 describe('Parse string to array', () => {
   it('should be able to parse and put in alphabetic order a string to array', async () => {
