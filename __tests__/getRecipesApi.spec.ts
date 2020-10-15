ff721b2524ad3704ccae927f42c97b8a54bc1380
Luiz Andre <luizxd234@gmail.com>
2020-10-15 11:09:18 -0300

    feat: git add .

---
+++ __tests__/getRecipesApi.spec.ts
@@ -0,0 +1,9 @@
+import recipesApi from '../src/modules/recipes/apis/recipePuppy';
+
+describe('Get recipes', () => {
+  it('should be able to get recipes', async () => {
+    const response = await recipesApi.get('/?i=onions');
+
+    expect(response.status).toBe(200);
+  });
+});
